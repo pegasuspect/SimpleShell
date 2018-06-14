01fdb1327828aad027ebc7da81b351be0bfe13aa
Osman Şekerlen <pegasuspect@users.noreply.github.com>
2018-06-14 14:35:56 -0400

    Now supporting private email addreses

--- git/init.sh
+++ git/init.sh
@@ -2,13 +2,25 @@
 # Description: This script sets your default git email to <github-email>,
 #   username to <username> and saves your password in osxkeychain to save you
 #   precious time.
-# Usage: ./init.sh <github-email> <username>
-if [[ -n "$1" && -n "$2" ]]; then
-  git config --global user.email $1
+# Usage: ./init.sh <github-email> <username> <isEmailPrivate>
+if [[ -n "$1" && -n "$2" && -n "$3"  ]]; then
+  if [[ -n "$3" ]]; then
+    git config --global user.email $2@users.noreply.github.com
+  else
+    git config --global user.email $1
+  fi
   git config --global user.name $2
   git config --global credential.helper osxkeychain
   git config --global push.default simple
   git config --list
 else
-  echo "Check the usage in $0"
+  echo "
+  Check usage below:
+  
+  Git Initialize
+  Description: This script sets your default git email to <github-email>,
+    username to <username> and saves your password in osxkeychain to save you
+    precious time.
+  Usage: ./init.sh <github-email> <username> <isEmailPrivate>
+  "
 fi
