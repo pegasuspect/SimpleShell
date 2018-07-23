2dd78071765caba535a65c3aea424fabd7461f61
Osman <1279406+pegasuspect@users.noreply.github.com>
2018-07-23 02:00:04 -0400

    Fixed it back to how it was.

--- git/configure_globally.sh
+++ git/configure_globally.sh
@@ -1,17 +1,9 @@
 if [[ -n "$1" && -n "$2" ]]; then
-  if [[ "$1" = "no" ]]; then
-    if [[ -n "$3" ]]; then
-      git config --global user.email $3
-    else
-      echo "You did not provide a 3rd argument as your email."
-      exit 1
-    fi
-  else
-    git config --global user.email $2@users.noreply.github.com
-  fi
-  git config --global user.name $2
-  git config --global credential.helper osxkeychain
+  git config --global user.email $2
+  git config --global user.name $1
+  git config --global --unset-all credential.helper
   git config --global --add credential.helper "cache --timeout 30000"
+  git config --global credential.helper osxkeychain
   # Fall back to 8 hours cache if not iOS
   git config --global push.default simple
   git config --list
@@ -20,11 +12,6 @@ else
   Git Configure
   Description: This script initializes your global configuration.
 
-  Usage: $0 <isEmailPrivate> <username> [github-email]
-
-  Options:
-    isEmailPrivate = yes|no
-
-  If your email is not private then provide it as the 3rd option/argument.
+  Usage: $0 <name> <github-email>
   "
 fi
